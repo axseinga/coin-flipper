5fd67869e092da116fb79e09166a836a9582ad40
Agnieszka Urbanowicz <urbanowicz.uk@gmail.com>
2021-08-23 18:31:36 +0100

    syntax fix after watching solution

--- src/CoinFlipper.js
+++ src/CoinFlipper.js
@@ -7,7 +7,7 @@ class CoinFlipper extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            coin: "",
+            coin: null,
             flips: 0,
             heads: 0,
             tails: 0,
@@ -15,34 +15,30 @@ class CoinFlipper extends React.Component {
         this.handleClick = this.handleClick.bind(this);
     }
 
-    countSides() {
-        this.state.coin === "heads"
-            ? this.setState({ tails: this.state.tails + 1 })
-            : this.setState({ heads: this.state.heads + 1 });
-    }
-
     flipCoin() {
+        this.setState((curState) => ({ flips: curState.flips + 1 }));
         const flipperCoin =
             this.props.sides[
                 Math.floor(Math.random() * this.props.sides.length)
             ];
         this.setState({ coin: flipperCoin });
+        flipperCoin === "heads"
+            ? this.setState({ heads: this.state.heads + 1 })
+            : this.setState({ tails: this.state.tails + 1 });
     }
 
     handleClick() {
-        this.setState((curState) => ({ flips: curState.flips + 1 }));
         this.flipCoin();
-        this.countSides();
     }
 
     render() {
         return (
             <div className="CoinFlipper">
                 <h1>Let's flip a coin!</h1>
-                <Coin side={this.state.coin} />
+                {this.state.coin && <Coin side={this.state.coin} />}
                 <button onClick={this.handleClick}>FLIP MEEE</button>
                 <p>
-                    Out of {this.state.flips} flips, there have been{" "}
+                    Out of {this.state.flips} flips, there have been
                     {this.state.heads} heads and {this.state.tails} tails.
                 </p>
             </div>
